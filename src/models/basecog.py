a8351b96de54716b7f22b79557a8d96038d43c5e
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-04-15 20:47:06 +0700

    mm

--- src/models/basecog.py
+++ src/models/basecog.py
@@ -8,10 +8,10 @@ class BaseCog(commands.Cog):
         return ctx.channel.id == 960446827579199488 \
             or 830875873027817484 in [role.id for role in ctx.author.roles] \
                 or 959723229805707285 in [role.id for role in ctx.author.roles] \
-                    or commands.is_owner(ctx.author)
+                    or self.bot.is_owner(ctx.author)
             
     def cog_application_command_check(self, interaction):
         return interaction.channel.id == 960446827579199488 \
             or 830875873027817484 in [role.id for role in interaction.user.roles] \
                 or 959723229805707285 in [role.id for role in interaction.user.roles] \
-                    or commands.is_owner(interaction.user)
+                    or self.bot.is_owner(interaction.user)
