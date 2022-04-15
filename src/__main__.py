ba7cc83b76e758664c7196b3090fcd61b5eb246c
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-04-15 22:03:50 +0700

    add meta

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,7 @@ class Impostor(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='sus ', intents=Intents.all())
         self.load_extension("exts.suggestion")
+        self.load_extension("exts.meta")
     
     
     async def on_ready(self):
