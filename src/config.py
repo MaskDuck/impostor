8e6f2c207be73ebcd62753256a728f4cfbe7d13f
Michael <56214343+RiskyMH@users.noreply.github.com>
2022-04-16 17:36:16 +1000

    remade ids shown & remade token direct use
    .

--- src/config.py
+++ src/config.py
@@ -8,10 +8,9 @@ if debug:
     load_dotenv(override=True)
 
 
-token = getenv("TOKEN")
-suggestion_channel = int(getenv("SUGGESTION_CHANNEL"))
-maintainer_role1 = int(getenv("MAINTAINER_ROLE1"))
-maintainer_role2 = int(getenv("MAINTAINER_ROLE2"))
-debug = bool(getenv("DEBUG"))
-pr_testing = bool(getenv("PR_TESTING"))
-
+# token = getenv("TOKEN")
+suggestion_channel = int(960446827579199488)
+maintainer_role1 = int(830875873027817484)
+maintainer_role2 = int(959723229805707285)
+debug = bool(getenv("DEBUG") or False)
+pr_testing = bool(getenv("PR_TESTING") or False)
