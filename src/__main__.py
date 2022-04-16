5a2955697728254e9a0aa1db875e8b2f81aaa1ed
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-04-16 23:23:48 +0700

    feat: env

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,11 @@
 from nextcord.ext import commands
 from nextcord import Intents
 
-debug = False
 
-if debug:
-    from dotenv import load_dotenv
 
-    load_dotenv(override=True)
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 
 class Impostor(commands.Bot):
@@ -14,6 +13,8 @@ class Impostor(commands.Bot):
         super().__init__(command_prefix="sus ", intents=Intents.all())
         self.load_extension("exts.suggestion")
         self.load_extension("exts.meta")
+        self.load_extension("exts.ping")
+
 
     async def on_ready(self):
         print("SUS")
