1cbd50b842d05bcabb3cf2e33174654f58bfe7bf
Michael <56214343+RiskyMH@users.noreply.github.com>
2022-04-17 18:11:57 +1000

    make better (change `utils` to use config) and ran formatter

--- src/config.py
+++ src/config.py
@@ -9,7 +9,9 @@ if debug:
 
 
 # token = getenv("TOKEN")
-suggestion_channel = int(960446827579199488)
+suggestion_channel = int(831425425510760478)
+bot_channel = int(960446827579199488)
+help_channel = int(946105325914828840)
 maintainer_role = int(830875873027817484)
 admin_role = int(959723229805707285)
 # Because it returns a string
