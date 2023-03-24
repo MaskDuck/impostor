274449a919b073971203eebcd2fdff485c180a47
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2023-03-24 20:44:55 +0700

    Update badge.py
    Signed-off-by: MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>

--- src/exts/badge.py
+++ src/exts/badge.py
@@ -1,8 +1,8 @@
 def setup(bot):
   @_bot.slash_command()
   async def test1(inter):
-    await inter.guild.create_auto_moderation_rule(name='test1',actions[AutoModerationAction(type=AutoModerationActionType.block_message)],trigger_metadata=AutoModerationTriggerMetadata(keyword_filter=["i hate maskduck"]))
+    await inter.guild.create_auto_moderation_rule(name='test1',actions=[AutoModerationAction(type=AutoModerationActionType.block_message)],trigger_metadata=AutoModerationTriggerMetadata(keyword_filter=["i hate maskduck"]))
 
   @_bot.slash_command()
   async def test2(inter):
-    await inter.guild.create_auto_moderation_rule(name='test2',actions[AutoModerationAction(type=AutoModerationActionType.block_message)],trigger_metadata=AutoModerationTriggerMetadata(keyword_filter=["i hate maskduck"]))
+    await inter.guild.create_auto_moderation_rule(name='test2',actions=[AutoModerationAction(type=AutoModerationActionType.block_message)],trigger_metadata=AutoModerationTriggerMetadata(keyword_filter=["i hate maskduck"]))
