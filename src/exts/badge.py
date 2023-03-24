80a0798b6a6bc71a06a0f5322d54f7dc962b9c01
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2023-03-24 20:46:42 +0700

    badge
    Signed-off-by: MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>

--- src/exts/badge.py
+++ src/exts/badge.py
@@ -1,5 +1,5 @@
 def setup(bot):
-    @_bot.slash_command()
+    @bot.slash_command()
     async def test1(inter):
         await inter.guild.create_auto_moderation_rule(
             name="test1",
@@ -9,7 +9,7 @@ def setup(bot):
             ),
         )
 
-    @_bot.slash_command()
+    @bot.slash_command()
     async def test2(inter):
         await inter.guild.create_auto_moderation_rule(
             name="test2",
